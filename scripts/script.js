8670096ead73009ae81403436f7ced7c92350700
Muhammed-Esmail <muhammedesmail101@gmail.com>
2026-03-24 01:14:47 +0200

    Better styling

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,12 @@ function clicked(val){
     else if(screen.innerText === error_message){
         screen.innerText = val;
     }
+    else if(val === "DEL"){
+        screen.innerText = screen.innerText.slice(0, -1);
+    }
     else{
         screen.innerText += val;
     }
 }
+
+console.log("MinOr".toLocaleLowerCase());
